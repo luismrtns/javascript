d3210dbe379873bdfd47d20693ce78053c4e0e61
Luís Martins <137302218+luismrtns@users.noreply.github.com>
2023-06-21 14:34:40 -0300

    Initial commit

---
+++ condições/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel>60){
+    console.log('multado fdp, se fudeu')
+}
+console.log('dirija com cuidado')
---
+++ condições/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`vivendo em ${país}`)
+if (país == 'brasil') {
+    console.log('você é brasileiro')
+} else{
+    console.log('você é estrangeiro');
+}
