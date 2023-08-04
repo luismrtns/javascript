7a80734d6155e76ffcf8cadd5ac697fa8743b50e
Luís Martins <137302218+luismrtns@users.noreply.github.com>
2023-08-04 17:08:09 -0300

    comecei os estudos sobre condições

---
+++ condições/ex008.js
@@ -0,0 +1,7 @@
+var vel = 40.5
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log('você ultrpassou o limite de velociade. O limite é de 60km/h. MULTADO!!!')
+}else{
+    console.log('dirija sempre usando cinto de segurança')
+}
