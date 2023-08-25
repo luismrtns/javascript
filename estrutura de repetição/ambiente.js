6b7282de0c833f1710c754220777948900e35c83
Luís Martins <137302218+luismrtns@users.noreply.github.com>
2023-08-25 17:52:30 -0300

    aprendi sobre as estruturas de repetições

---
+++ estrutura de repetição/ambiente.js
@@ -0,0 +1,24 @@
+/*var c = 1
+while(c <= 6){
+    console.log(`número ${c}`)
+    c+=1
+}*/
+
+/*var cont = 1
+do{
+    console.log(`número ${cont}`)
+    cont+=1
+}while(cont<=6)*/
+
+console.log('número pares entre 0 e 10: ')
+for (var c=0;c<=10;c+=2){
+    console.log(c)
+}
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
+
